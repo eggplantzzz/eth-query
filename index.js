666dac46fddd3021856bc235d953c83c0890e8c7
eggplantzzz <pobol234@gmail.com>
2018-07-04 16:11:56 -0700

    Instead of requiring web3 in lib, require it at the top level and pass it as an argument to all functions needing it

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const Web3 = require('web3');
+const { infuraApiKey } = require('./config');
+const web3 = new Web3('https://mainnet.infura.io/' + infuraApiKey);
 const {
   aggregateAllBlocksData,
   calculateRelevantBlockNumbers,
@@ -9,12 +12,12 @@ const {
 
 const etherReportFromLastWrittenBlocks = (numberOfBlocks) => {
   if (numberOfBlocks <= 0) throw new Error('The number of transactions must be positive.');
-  calculateRelevantBlockNumbers(numberOfBlocks)
-    .then(collectDataOnBlocks)
-    .then(aggregateAllBlocksData)
+  calculateRelevantBlockNumbers(numberOfBlocks, web3)
+    .then(relevantBlockNumbers => collectDataOnBlocks(relevantBlockNumbers, web3))
+    .then(blocksData => aggregateAllBlocksData(blocksData, web3))
     .then(filterDuplicateAddresses)
-    .then(determineAllContractAddresses)
-    .then(filteredAggregatedData => createReport(numberOfBlocks, filteredAggregatedData))
+    .then(filteredAggregatedData => determineAllContractAddresses(filteredAggregatedData, web3))
+    .then(preparedData => createReport(numberOfBlocks, preparedData))
     .catch(error => console.log(error));
 }
 
